4b966a4c3dc868cd1a2475c11764c30bbb02f5d6
SimoBor99 <100366282+SimoBor99@users.noreply.github.com>
2023-01-20 16:22:46 +0000

    Add files via upload

---
+++ following_goal/src/publisher.cpp
@@ -0,0 +1,58 @@
+#include "ros/ros.h"
+#include <unistd.h>
+#include "nav_msgs/Odometry.h"
+#include "following_goal/Robot_pv.h"
+
+// global variable
+float posx, posy, vx, vy;
+ros::Publisher pub;
+
+void pos_v_Callback(const nav_msgs::Odometry::ConstPtr& msg) {	
+	
+	// set the rate
+	ros::Rate rate(1);
+	
+	ROS_INFO("Robot pos and vel:[%f, %f, %f, %f]", msg->pose.pose.position.x, msg->pose.pose.position.y, msg->twist.twist.linear.x, msg->twist.twist.linear.y);
+	rate.sleep();
+	/* subscribe position and 
+	velocity from topic odom */
+	posx=msg->pose.pose.position.x;
+	posy=msg->pose.pose.position.y;
+	vx=msg->twist.twist.linear.x;
+	vy=msg->twist.twist.linear.y;
+	following_goal::Robot_pv pv;
+	
+	/* send position and velocity 
+	on topic info_robot*/
+	pv.posx=posx;
+	pv.posy=posy;
+	pv.velx=vx;
+	pv.vely=vy;
+	
+	// publish the information
+	pub.publish(pv);
+}
+
+int main(int argc, char ** argv) {
+
+	/* you must call one of the versions of ros::init() before using any other
+  	part of the ROS system. */
+	ros::init(argc, argv, "robot_publisher");
+
+	// NodeHandle is the main access point to communications with the ROS system.
+	ros::NodeHandle n;
+	
+	/* this function tells how is the subscriber:
+		the first argument tells the topic where it takes the information
+		the second one is the size of message queue
+		the third is the callback that is called each time */
+	ros::Subscriber sub=n.subscribe("/odom", 1, pos_v_Callback);
+	
+	/* this function tells how is the publisher:
+		the first argument tells the topic where it publish the information
+		the second one is the size of message queue */
+	pub=n.advertise<following_goal::Robot_pv>("/info_robot", 1);
+
+	ros::spin();
+	return 0;
+}
